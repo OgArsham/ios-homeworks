a9774d22a6e5da3bb5ff1c2ae266a4a82b9b1c91
OgArsham <arsham29@gmail.com>
2023-05-24 15:22:41 +0400

    HW 1

---
+++ Navigation/Controllers/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  profileViewController.swift
+//  Navigation
+//
+//  Created by Arsham Oganesian on 23.05.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .cyan
+        title = "My profile"
+        
+    
+       
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
